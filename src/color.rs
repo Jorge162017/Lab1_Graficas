1f0275aedbe0a9108128306c88e0fed7bfd435c0
Jorge162017 <lop221038@uvg.edu.gt>
2024-07-20 23:39:45 -0600

    Cuarto poligono terminado

--- src/color.rs
+++ src/color.rs
@@ -11,5 +11,6 @@ impl Color {
     pub const BLACK: Color = Color { r: 0, g: 0, b: 0 };
     pub const BLUE: Color = Color { r: 0, g: 0, b: 255 };
     pub const RED: Color = Color { r: 255, g: 0, b: 0 };
+    pub const GREEN: Color = Color { r: 0, g: 255, b: 0 };
 }
 
